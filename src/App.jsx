98c5e3c8139efe11af52e8c3dc4fd35afb7c0dc6
kyeong-bin <kyeongb0315@gmail.com>
2025-04-15 12:32:23 +0900

    feat: 페이지 라우팅 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
-        <>
-            <h1>first commit</h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
     );
 }
 
