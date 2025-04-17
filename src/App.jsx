28e4144c1bf069d0a89017662673b1377d181b1e
kyeong-bin <kyeongb0315@gmail.com>
2025-04-17 14:32:10 +0900

    feat: 카카오/애플 로그인 콜백 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import NotFound from "./pages/NotFound";
 import PhoneAuthPage from "./pages/PhoneAuth";
+import KakaoCallback from "./pages/callback/KakaoCallback";
+import AppleCallBack from "./pages/callback/AppleCallback";
 
 function App() {
     return (
@@ -15,6 +17,14 @@ function App() {
             <Route
                 path="/phoneauth"
                 element={<PhoneAuthPage />}
+            />
+            <Route
+                path="/oauth/kakao/callback"
+                element={<KakaoCallback />}
+            />
+            <Route
+                path="/auth/apple/callback"
+                element={<AppleCallBack />}
             />
             <Route path="*" element={<NotFound />} />
         </Routes>
