af5ff79c770b7619c2deb4e3f3a1cf86b463958e
kyeong-bin <kyeongb0315@gmail.com>
2025-04-25 13:44:49 +0900

    feat: Zustand를 활용한 인증 상태 관리 스토어 구현

---
+++ src/store/authStore.js
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+const useAuthStore = create(
+    persist(
+        (set) => ({
+            // 인증 관련 상태
+            accessToken: "",
+            refreshToken: "",
+            authorizationId: "",
+            scope: "",
+            isLoggedIn: false,
+
+            // 로그인 액션
+            setAuth: (authData) =>
+                set({
+                    accessToken: authData.accessToken,
+                    refreshToken: authData.refreshToken,
+                    authorizationId:
+                        authData.authorizationId,
+                    scope: authData.scope,
+                    isLoggedIn: true,
+                }),
+
+            // 로그아웃 액션
+            logout: () =>
+                set({
+                    accessToken: "",
+                    refreshToken: "",
+                    authorizationId: "",
+                    scope: "",
+                    isLoggedIn: false,
+                }),
+        }),
+        {
+            name: "auth-storage",
+            getStorage: () => localStorage,
+        }
+    )
+);
+
+export default useAuthStore;
