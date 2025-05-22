357ef944aa828efc2c6d44bbc416ec9cacb56695
kyeong-bin <kyeongb0315@gmail.com>
2025-05-22 16:53:21 +0900

    feat: 날짜, 결제 타입, 숫자 포매팅 유틸 함수 추가

---
+++ src/utils/paymentTypeToKorean.js
@@ -0,0 +1,21 @@
+/**
+ * paymentType 값을 한국어로 변환
+ * @param {string} type - 예: "HOURLY", "MONTHLY", "YEARLY" 등
+ * @returns {string} - 예: "시급", "월급", "연봉" 등
+ */
+export function paymentTypeToKorean(type) {
+    switch (type) {
+        case "HOURLY":
+            return "시급";
+        case "DAILY":
+            return "일급";
+        case "WEEKLY":
+            return "주급";
+        case "MONTHLY":
+            return "월급";
+        case "YEARLY":
+            return "연봉";
+        default:
+            return type; // 알 수 없는 값은 그대로 반환
+    }
+}
