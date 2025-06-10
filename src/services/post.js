06a99a466174f510c0a6fe3331792b90ceafbf3e
kyeong-bin <kyeongb0315@gmail.com>
2025-06-10 11:32:35 +0900

    feat: 공고 지원 및 스크랩 관련 로직 구현

--- src/services/post.js
+++ src/services/post.js
@@ -115,3 +115,125 @@ export const getAvailableKeywords = async () => {
         );
     }
 };
+
+// 공고 지원 로직
+export const postingApply = async ({
+    postingId,
+    description,
+    postingScheduleId,
+}) => {
+    try {
+        const response = await fetch(
+            `${backend}/app/postings/apply/${postingId}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify({
+                    postingScheduleId,
+                    description,
+                }),
+            }
+        );
+        console.log(postingScheduleId, description);
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('공고 지원 중 오류:', error);
+        throw new Error(
+            '공고 지원 중 오류가 발생했습니다.'
+        );
+    }
+};
+
+// 공고 스크랩 등록 로직
+export const addPostingScrap = async ({ postingId }) => {
+    try {
+        const response = await fetch(
+            `${backend}/app/users/me/postings/favorites/${postingId}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('공고 스크랩 등록 중 오류:', error);
+        throw new Error(
+            '공고 스크랩 등록 중 오류가 발생했습니다.'
+        );
+    }
+};
+
+// 공고 스크랩 삭제 로직
+export const deletePostingScrap = async ({
+    favoritePostingId,
+}) => {
+    try {
+        const response = await fetch(
+            `${backend}/app/users/me/postings/favorites/${favoritePostingId}`,
+            {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('공고 스크랩 삭제 중 오류:', error);
+        throw new Error(
+            '공고 스크랩 삭제 중 오류가 발생했습니다.'
+        );
+    }
+};
+
+// 공고 스크랩 목록 조회 로직
+export const getPostingScrapList = async ({
+    cursorInfo,
+}) => {
+    try {
+        const response = await fetch(
+            `${backend}/app/users/me/postings/favorites?cursor=${cursorInfo}&pageSize=10`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('공고 리스트 조회 오류:', error);
+        throw new Error(
+            '공고 리스트 조회 중 오류가 발생했습니다.'
+        );
+    }
+};
