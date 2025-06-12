2d2abcd775af7435f511379cb1dae9ba80b2fd7d
kyeong-bin <kyeongb0315@gmail.com>
2025-06-12 17:06:08 +0900

    feat: 공고 지원 목록 조회 및 지원 취소 로직 구현

--- src/services/myPage.js
+++ src/services/myPage.js
@@ -33,3 +33,66 @@ export const getScrapPostList = async ({ cursorInfo }) => {
         );
     }
 };
+
+// 공고 지원 목록 조회 로직
+export const getApplicationList = async ({
+    page = 1,
+    pageSize = 10,
+}) => {
+    try {
+        const response = await fetch(
+            `${backend}/app/users/me/postings/applications?page=${page}&pageSize=${pageSize}`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('공고 지원 목록 조회 오류:', error);
+        throw new Error(
+            '공고 지원 목록 조회 중 오류가 발생했습니다.'
+        );
+    }
+};
+
+// 공고 지원 취소 로직
+export const cancelApplication = async ({
+    applicationId,
+}) => {
+    try {
+        const response = await fetch(
+            `${backend}/app/users/me/postings/applications/${applicationId}/status`,
+            {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+                body: JSON.stringify({
+                    status: 'CANCELLED',
+                }),
+            }
+        );
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('공고 지원 취소 중 오류:', error);
+        throw new Error(
+            '공고 지원 취소 중 오류가 발생했습니다.'
+        );
+    }
+};
