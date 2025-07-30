bd45cca86b43d115feb3234e9ce211f6007541fc
kyeong-bin <kyeongb0315@gmail.com>
2025-07-30 15:42:56 +0900

    feat: 업장 목록 및 지원자 목록 조회 로직 추가

---
+++ src/services/mainPageService.js
@@ -0,0 +1,61 @@
+import useAuthStore from '../store/authStore';
+
+const backend = import.meta.env.VITE_API_URL;
+
+// 업장 목록 조회 로직
+export const getWorkplaceList = async () => {
+    const accessToken = useAuthStore.getState().accessToken;
+    try {
+        const response = await fetch(
+            `${backend}/manager/workspaces`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('업장 목록 조회 오류:', error);
+        throw new Error(
+            '업장 목록 조회 중 오류가 발생했습니다.'
+        );
+    }
+};
+
+// 지원자 목록 조회 로직
+export const getApplicants = async () => {
+    const accessToken = useAuthStore.getState().accessToken;
+    try {
+        const response = await fetch(
+            `${backend}/manager/postings/applications?pageSize=5`,
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error('서버 응답 오류');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('지원자 목록 조회 오류:', error);
+        throw new Error(
+            '지원자 목록 조회 중 오류가 발생했습니다.'
+        );
+    }
+};
