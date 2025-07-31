ef86791a5c32d84c9f62276e44e9090c051cfeb4
kyeong-bin <kyeongb0315@gmail.com>
2025-07-31 15:16:50 +0900

    fix: Loader 컴포넌트 레이아웃 수정

--- src/pages/callback/KakaoCallback.jsx
+++ src/pages/callback/KakaoCallback.jsx
@@ -32,7 +32,9 @@ export default function KakaoCallback() {
 }
 
 const Container = styled.div`
+    height: calc(100vh - 81px);
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
 `;
